95807a11f4ce5f6177795ec8a07dc4cc87e9b4b5
LeeJinWoo <108853443+devhomh@users.noreply.github.com>
2024-05-10 18:10:28 +0900

    [Gold IV] Title: 세 수의 합, Time: 980 ms, Memory: 32592 KB -BaekjoonHub

---
+++ 백준/Gold/2295. 세 수의 합/세 수의 합.java
@@ -0,0 +1,38 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int num = Integer.parseInt(br.readLine());
+        int[] arr = new int[num];
+        for (int i = 0; i < num; i++) {
+            arr[i] = Integer.parseInt(br.readLine());
+        }
+
+        int[] sum = new int[num * num];
+        int count = 0;
+        for (int i = 0; i < num; i++) {
+            for (int j = 0; j < num; j++) {
+                sum[count] = arr[i] + arr[j];
+                count++;
+            }
+        }
+
+        Arrays.sort(sum);
+        int max = Integer.MIN_VALUE;
+
+        for (int i = 0; i < num; i++) {
+            for (int j = 0; j < num; j++) {
+                if (Arrays.binarySearch(sum, arr[i] - arr[j]) >= 0) {
+                    max = Math.max(max, arr[i]);
+                }
+            }
+        }
+
+        System.out.println(max);
+    }
+}
