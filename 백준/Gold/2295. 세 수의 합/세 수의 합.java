e09cb7f73f7bfc70ab40d9b0386ae9b82ea9c89e
LeeJinWoo <108853443+devhomh@users.noreply.github.com>
2024-05-10 18:12:25 +0900

    [Gold IV] Title: 세 수의 합, Time: 896 ms, Memory: 32636 KB -BaekjoonHub

--- 백준/Gold/2295. 세 수의 합/세 수의 합.java
+++ 백준/Gold/2295. 세 수의 합/세 수의 합.java
@@ -25,8 +25,8 @@ public class Main {
         Arrays.sort(sum);
         int max = Integer.MIN_VALUE;
 
-        for (int i = 0; i < num; i++) {
-            for (int j = 0; j < num; j++) {
+        for (int i = num - 1; i >= 0; i--) {
+            for (int j = i; j >= 0; j--) {
                 if (Arrays.binarySearch(sum, arr[i] - arr[j]) >= 0) {
                     max = Math.max(max, arr[i]);
                 }
