9e809fe01e59b66b79db1369e1e98aa02e80e778
LeeJinWoo <108853443+devhomh@users.noreply.github.com>
2024-05-20 14:59:17 +0900

    [Silver I] Title: 포도주 시식, Time: 148 ms, Memory: 14968 KB -BaekjoonHub

--- 백준/Silver/2156. 포도주 시식/포도주 시식.java
+++ 백준/Silver/2156. 포도주 시식/포도주 시식.java
@@ -6,15 +6,17 @@ public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
         int num = Integer.parseInt(input.readLine());
-        int[] wines = new int[10001];
-        int[] dp = new int[10001];
+        int[] wines = new int[num + 1];
+        int[] dp = new int[num + 1];
         for (int i = 1; i <= num; i++) {
             wines[i] = Integer.parseInt(input.readLine());
         }
 
         dp[0] = 0;
         dp[1] = wines[1];
-        dp[2] = wines[1] + wines[2];
+        if (num >= 2) {
+            dp[2] = wines[1] + wines[2];
+        }
         for (int i = 3; i <= num; i++) {
             dp[i] = Math.max(dp[i - 1], Math.max(dp[i - 2] + wines[i], dp[i - 3] + wines[i - 1] + wines[i]));
         }
