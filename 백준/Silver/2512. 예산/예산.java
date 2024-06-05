22b7e02882faa7d1ba1173237272aa56324e3f9b
LeeJinWoo <108853443+devhomh@users.noreply.github.com>
2024-06-05 10:42:12 +0900

    [Silver II] Title: 예산, Time: 180 ms, Memory: 15644 KB -BaekjoonHub

--- 백준/Silver/2512. 예산/예산.java
+++ 백준/Silver/2512. 예산/예산.java
@@ -1,37 +1,38 @@
+import javax.print.attribute.IntegerSyntax;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.util.*;
+import java.util.Arrays;
+import java.util.StringTokenizer;
 import java.util.stream.IntStream;
 
 public class Main {
-    static int totalBudget;
+    static int num, limit;
     static int[] regions;
 
     public static void main(String[] args) throws IOException {
         BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
-        int num = Integer.parseInt(input.readLine());
-        StringTokenizer line = new StringTokenizer(input.readLine());
-
+        num = Integer.parseInt(input.readLine());
         regions = new int[num];
+        StringTokenizer line = new StringTokenizer(input.readLine());
         for (int i = 0; i < num; i++) {
             regions[i] = Integer.parseInt(line.nextToken());
         }
 
-        totalBudget = Integer.parseInt(input.readLine());
-
+        limit = Integer.parseInt(input.readLine());
         Arrays.sort(regions);
+
         int low = 1;
         int high = regions[num - 1];
-        if (IntStream.of(regions).sum() <= totalBudget) {
+
+        if (IntStream.of(regions).sum() <= limit) {
             System.out.println(high);
             return;
         }
 
         while (low + 1 < high) {
-            int mid = (high + low) / 2;
-
-            if (isWithinBudget(mid)) {
+            int mid = (low + high) / 2;
+            if (possible(mid)) {
                 low = mid;
             } else {
                 high = mid;
@@ -41,12 +42,12 @@ public class Main {
         System.out.println(low);
     }
 
-    public static boolean isWithinBudget(int boundary) {
-        int result = 0;
-        for (int region : regions) {
-            result += Math.min(boundary, region);
+    public static boolean possible(int target) {
+        int sum = 0;
+        for (int i = 0; i < num; i++) {
+            sum += Math.min(target, regions[i]);
         }
 
-        return result <= totalBudget;
+        return sum <= limit;
     }
 }
