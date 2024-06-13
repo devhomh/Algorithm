848508305027d4a150a0ad59eeffbe321f178305
LeeJinWoo <108853443+devhomh@users.noreply.github.com>
2024-06-13 15:02:05 +0900

    [Gold I] Title: 구슬 탈출 2, Time: 124 ms, Memory: 14420 KB -BaekjoonHub

--- 백준/Gold/13460. 구슬 탈출 2/구슬 탈출 2.java
+++ 백준/Gold/13460. 구슬 탈출 2/구슬 탈출 2.java
@@ -1,3 +1,4 @@
+import java.awt.*;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
@@ -6,152 +7,131 @@ import java.util.Queue;
 import java.util.StringTokenizer;
 
 public class Main {
-	static int N, M;
-	static char[][] map;
-	static boolean[][][][] visited;
-	static int holeX, holeY;
-	static Marble blue, red;
-	
-	static int[] dx = {-1, 0, 1, 0};
-	static int[] dy = {0, 1, 0, -1}; //0, 1, 2, 3 (상, 우, 하, 좌) - 시계 방향 
+    static int[] dx = {-1, 1, 0, 0};
+    static int[] dy = {0, 0, -1, 1};
+    static int row, column;
+    static Point hole;
+    static String[][] board;
+    static boolean[][][][] visit;
 
-	public static void main(String[] args) throws IOException {
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		StringTokenizer st = new StringTokenizer(br.readLine());
+    public static void main(String[] args) throws IOException {
+        BufferedReader input = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer condition = new StringTokenizer(input.readLine());
+        row = Integer.parseInt(condition.nextToken());
+        column = Integer.parseInt(condition.nextToken());
+        visit = new boolean[row][column][row][column];
+        board = new String[row][column];
+        int[] start = new int[5];
+        for (int i = 0; i < row; i++) {
+            String line = input.readLine();
+            for (int j = 0; j < column; j++) {
+                String element = Character.toString(line.charAt(j));
+                if (element.equals("O")) {
+                    hole = new Point(i, j);
+                }
 
-		N = Integer.parseInt(st.nextToken());		
-		M = Integer.parseInt(st.nextToken());		
-		
-		map = new char[N][M];
-		visited = new boolean[N][M][N][M];
+                if (element.equals("R")) {
+                    start[0] = i;
+                    start[1] = j;
+                }
 
-		// 구슬 map 구성 
-		for(int i = 0; i < N; i++) {
-			String str = br.readLine();
-			for(int j = 0; j < M; j++) {
-				map[i][j] = str.charAt(j);
-				
-				if(map[i][j] == 'O') {
-					holeX = i;
-					holeY = j;
-				} else if(map[i][j] == 'B') {
-					blue = new Marble(0, 0, i, j, 0);
-				} else if(map[i][j] == 'R') {
-					red = new Marble(i, j, 0, 0, 0);
-				}
-			}
-		}
+                if (element.equals("B")) {
+                    start[2] = i;
+                    start[3] = j;
+                }
 
-		System.out.println(bfs());
-		
-		br.close();
-	}
-	
-	public static int bfs() {
-		Queue<Marble> queue = new LinkedList<>();
-		queue.add(new Marble(red.rx, red.ry, blue.bx, blue.by, 1));
-		visited[red.rx][red.ry][blue.rx][blue.ry] = true;
-		
-		while(!queue.isEmpty()) {
-			Marble marble = queue.poll();
-			
-			int curRx = marble.rx;
-			int curRy = marble.ry;
-			int curBx = marble.bx;
-			int curBy = marble.by;
-			int curCnt = marble.cnt;
-		
-			if(curCnt > 10) { // 이동 횟수가 10 초과시 실패 
-				return -1;
-			}
-			
-			for(int i = 0; i < 4; i++) {
-				int newRx = curRx;
-				int newRy = curRy;
-				int newBx = curBx;
-				int newBy = curBy;
-				
-				boolean isRedHole = false;
-				boolean isBlueHole = false;
-				
-				// 빨간 구슬 이동 -> # 벽을 만날 때까지 이동 
-				while(map[newRx + dx[i]][newRy + dy[i]] != '#') { 
-					newRx += dx[i];
-					newRy += dy[i];
-					
-					// 이동 중 구멍을 만날 경우 
-					if(newRx == holeX && newRy == holeY) { 
-						isRedHole = true;
-						break;
-					}
-				}
-				
-				// 파란 구슬 이동 -> # 벽을 만날 때까지 이동 
-				while(map[newBx + dx[i]][newBy + dy[i]] != '#') { 
-					newBx += dx[i];
-					newBy += dy[i];
-					
-					// 이동 중 구멍을 만날 경우 
-					if(newBx == holeX && newBy == holeY) { 
-						isBlueHole = true;
-						break;
-					}
-				}
-				
-				if(isBlueHole) { // 파란 구슬이 구멍에 빠지면 무조건 실패 
-					continue; // 하지만 큐에 남은 다른 좌표도 봐야하니 다음으로 
-				}
-				
-				if(isRedHole && !isBlueHole) { // 빨간 구슬만 구멍에 빠지면 성공 
-					return curCnt;
-				}
-				
-				// 둘 다 구멍에 빠지지 않았는데 이동할 위치가 같은 경우 -> 위치 조정
-				if(newRx == newBx && newRy == newBy) {
-					if(i == 0) { // 위쪽으로 기울이기 
-						// 더 큰 x값을 가지는 구슬이 뒤로 감 
-						if(curRx > curBx) newRx -= dx[i]; 
-						else newBx -= dx[i];
-					} else if(i == 1) { // 오른쪽으로 기울이기 
-						// 더 작은 y값을 가지는 구슬이 뒤로 감 
-						if(curRy < curBy) newRy -= dy[i];
-						else newBy -= dy[i];	
-					} else if(i == 2) { // 아래쪽으로 기울이기 
-						// 더 작은 x값을 가지는 구슬이 뒤로 감 
-						if(curRx < curBx) newRx -= dx[i]; 
-						else newBx -= dx[i];
-					} else { // 왼쪽으로 기울이기 
-						// 더 큰 y값을 가지는 구슬이 뒤로 감 
-						if(curRy > curBy) newRy -= dy[i]; 
-						else newBy -= dy[i];	
-					}
-				}
-				
-				// 두 구슬이 이동할 위치가 처음 방문하는 곳인 경우만 이동 -> 큐에 추가 
-				if(!visited[newRx][newRy][newBx][newBy]) {
-					visited[newRx][newRy][newBx][newBy] = true;
-					queue.add(new Marble(newRx, newRy, newBx, newBy, curCnt+1));
-				}
-			}
-		}
-		
-		return -1;
-	}
+                board[i][j] = element;
+            }
+        }
 
-}
+        System.out.println(bfs(start));
+    }
 
-class Marble {
-	int rx;
-	int ry;
-	int bx;
-	int by;
-	int cnt;
-	
-	Marble(int rx, int ry, int bx, int by, int cnt) {
-		this.rx = rx;
-		this.ry = ry;
-		this.bx = bx;
-		this.by = by;
-		this.cnt = cnt;
-	}
+    public static int bfs(int[] start) {
+        Queue<int[]> queue = new LinkedList<>();
+        start[4] = 1;
+        queue.add(start);
+        visit[start[0]][start[1]][start[2]][start[3]] = true;
+
+        while (!queue.isEmpty()) {
+            int[] current = queue.poll();
+
+            if (current[4] > 10) {
+                return -1;
+            }
+
+            for (int i = 0; i < 4; i++) {
+                int redRow = current[0];
+                int redColumn = current[1];
+                int blueRow = current[2];
+                int blueColumn = current[3];
+                boolean passRed = false;
+                boolean passBlue = false;
+
+                while (!board[redRow + dx[i]][redColumn + dy[i]].equals("#")) {
+                    redRow += dx[i];
+                    redColumn += dy[i];
+
+                    if (redRow == hole.x && redColumn == hole.y) {
+                        passRed = true;
+                        break;
+                    }
+                }
+
+                while (!board[blueRow + dx[i]][blueColumn + dy[i]].equals("#")) {
+                    blueRow += dx[i];
+                    blueColumn += dy[i];
+
+                    if (blueRow == hole.x && blueColumn == hole.y) {
+                        passBlue = true;
+                        break;
+                    }
+                }
+
+                if (passBlue) {
+                    continue;
+                }
+
+                if (passRed && !passBlue) {
+                    return current[4];
+                }
+
+
+                if(redRow == blueRow && redColumn == blueColumn) {
+                    if (i == 0) {
+                        if (current[0] > current[2]) {
+                            redRow -= dx[i];
+                        } else {
+                            blueRow -= dx[i];
+                        }
+                    } else if (i == 1) {
+                        if (current[0] > current[2]) {
+                            blueRow -= dx[i];
+                        } else {
+                            redRow -= dx[i];
+                        }
+                    } else if (i == 2) {
+                        if (current[1] > current[3]) {
+                            redColumn -= dy[i];
+                        } else {
+                            blueColumn -= dy[i];
+                        }
+                    } else {
+                        if (current[1] > current[3]) {
+                            blueColumn -= dy[i];
+                        } else {
+                            redColumn -= dy[i];
+                        }
+                    }
+                }
+
+                if (!visit[redRow][redColumn][blueRow][blueColumn]) {
+                    visit[redRow][redColumn][blueRow][blueColumn] = true;
+                    queue.add(new int[] {redRow, redColumn, blueRow, blueColumn, current[4] + 1});
+                }
+            }
+        }
+
+        return -1;
+    }
 }
